1fbcb3b243292603afb041bbfe2d9c78801f77ee
KRISZTIAN NAGY - Student <G00330024@gmit.ie>
2017-10-31 14:50:35 +0000

    Send text on enter press. Set initial focus onto text area

--- www/js/index.js
+++ www/js/index.js
@@ -3,11 +3,17 @@ var serverUrl="http://localhost:8080/ajax/";
 
 //Add the click event to the button
 window.onload = function() {
+    //Register click event listener onto the send button
     document.getElementById("sendButton").addEventListener('click',sendButtonClick);
+    //Register the enter pressed event onto the text box
+    document.getElementById("newwrittenmessage").addEventListener('keypress',sendOnEnterPress);
+    //Set the focus to the textarea
+    document.getElementById("newwrittenmessage").focus();
     //Start conversation
     //sendButtonClick();
 }
 
+//Function used to send the ajax request
 function sendRequest(){
     var request = new XMLHttpRequest();
 
@@ -46,7 +52,7 @@ function sendRequest(){
     };
 
     request.onerror = function() {
-        console.log(request);
+        //console.log(request);
         // There was a connection error of some sort
         //start animation
             sendandreceivedAnimation();
@@ -55,7 +61,7 @@ function sendRequest(){
     //Send the data
     request.send(JSON.stringify({"Question":textarea.value}));
 }
-
+//Function used to add a chat bubble
 function addBubble(text,owner){
     var side="left";
     //Change the side if the owner is the user
@@ -78,7 +84,7 @@ function addBubble(text,owner){
     parentContainer.appendChild(messageParentDiv);
 }
 
-
+//Function used to caputure the button click
 function sendButtonClick(){
     //Get the textarea
     var textarea=document.getElementById("newwrittenmessage");
@@ -86,20 +92,36 @@ function sendButtonClick(){
     if(textarea.value.trim()!=""){
         //Send the request
         sendRequest();
+        //Set the focus back onto the textarea
+        textarea.focus();
+    }
+}
+//Function used to send the text on enter press
+function sendOnEnterPress(event){
+    //If enter was pressed
+    if(event.charCode==13){
+        //Check if there is anything to send
+        if(event.target.value.trim()!=""){
+            //Send the request
+            sendRequest();
+        }
     }
 }
 
-
+//Function used to change the send button into a loading animation, and back
 function sendandreceivedAnimation(){
     var text=document.getElementById("sendtext");
     var loading=document.getElementById("loading");
     var textarea=document.getElementById("newwrittenmessage");
 
+    //If the text us hidden
     if(text.classList.contains("hidden")){
+        //Make it visible
         text.classList.remove("hidden");
         loading.classList.add("hidden");
         //textarea.disabled=true;
     }else{
+        //Make it hidden
         textarea.disabled=false;
         text.classList.add("hidden");
         loading.classList.remove("hidden");
